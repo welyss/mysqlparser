782deb96f9a22bf8c1d87cd784668aeea0b9ab45
welyss <welyss@163.com>
2023-04-21 14:09:15 +0800

    take alter info bug fixed

--- src/main/java/org/welyss/mysqlparser/MyParser.java
+++ src/main/java/org/welyss/mysqlparser/MyParser.java
@@ -10048,7 +10048,7 @@ class MyParser {
 				// lex->name.str= 0;
 				// lex->name.length= 0;
 				thd.lex.sqlCommand = SQLCommand.SQLCOM_ALTER_TABLE;
-				thd.lex.mSqlCmd = thd.sql.substring(myLexer.lip.getPtr(thd));
+				thd.lex.mSqlCmd = thd.sql.substring(thd.mTokStart);
 				// lex->duplicates= DUP_ERROR;
 				if (!MyParserProcessor.addTableToList(thd, (TableIdent) yystack.valueAt(4 - (4)), null, null))
 					return YYABORT;
